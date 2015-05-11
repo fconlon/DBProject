66f196d1203ec691ce83a0145808234595185897
fconlon <thibarol@yahoo.com>
2015-05-10 19:54:55 -0500

    Query D done

---
+++ PHP/view_course_offerings.php
@@ -0,0 +1,24 @@
+<?php
+include 'printTables.php';
+
+// information about the database you will connect to
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "dbproject";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT c.course_code, title, semester, year, room, current, name
+        FROM section_info s, enrollment e, instr_assign ia, instructor i, course c
+        WHERE  s.crn=e.crn AND ia.crn=s.crn AND id=iid AND s.course_code=c.course_code AND year>2015-$_POST[yearsBack] AND c.course_code=\"$_POST[code]\";";
+
+printQuery($conn, "Special Courses", $sql);
+
+$conn->close();
+?>
